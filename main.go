99c6d2580f9f7d1553e911f168af0114023f499c
sbandari@newrelic.com <sbandari@newrelic.com>
2021-11-18 23:47:09 +0530

    updated main.go

--- main.go
+++ main.go
@@ -2,21 +2,38 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
-func resourceCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+func dataSourceCoffesRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := &http.Client{Timeout: 10 * time.Second}
 	var diags diag.Diagnostics
-	id := d.Get("id").(string)
-	resp, err := http.Post("http://localhost:8080/albums", id, nil)
+	req, err := http.NewRequest("Get", fmt.Sprintf("%s/albums", "http://localhost:8080"), nil)
 	if err != nil {
-		diag.FromErr(err)
+		return diag.FromErr(err)
 	}
-	defer resp.Body.Close()
+	r, err := client.Do(req)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	defer r.Body.Close()
+	albums := make([]map[string]interface{}, 0)
+	err = json.NewDecoder(r.Body).Decode(&albums)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("albums", albums); err != nil {
+		return diag.FromErr(err)
+	}
+	d.SetId(strconv.FormatInt(time.Now().Unix(), 10))
 	return diags
 }
 
@@ -28,7 +45,7 @@ func resource_item() *schema.Resource {
 				Required: true,
 			},
 		},
-		CreateContext: resourceCreate,
+		ReadContext: dataSourceCoffesRead,
 	}
 }
 
@@ -51,6 +68,7 @@ func provider() *schema.Provider {
 		},
 	}
 }
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: provider,
